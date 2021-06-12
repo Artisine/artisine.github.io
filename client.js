1c364e2dbaf50959b2b1bcc4191ea5b38269c631
ghostwalker13 <artisinedev@gmail.com>
2021-06-12 23:43:41 +0100

    0.8.4 In progress making scroll detection for each Article in left side

--- client.js
+++ client.js
@@ -13,16 +13,38 @@ const portfolioElements = {
 // portfolioElements.header.style.marginTop = `calc(50vh - ${portfolioElements.verticalList.clientHeight}px)`;
 portfolioElements.header.style.top = `${portfolioElements.header.clientHeight/2}px`;
 
+const leftSection = getElement(`section#left`);
+const rightSection = getElement(`section#right`);
+
+const articles = [...leftSection.children].filter((child) => child.localName === "article" && child.classList.contains("portfolio"));
+console.log(articles);
+
+console.log(leftSection.offsetTop);
+
+function when_scroll(e) {
+	const evt = e || event;
+	// console.log(evt);
+	// leftSection
+	// console.log(`${leftSection.scrollTop}`);
+}
+
+leftSection.addEventListener("scroll", when_scroll);
+// rightSection.addEventListener("scroll", when_scroll);
 
 
+function something() {
 
+	articles.forEach((arti) => {
+		if (leftSection.scrollTop >= arti.offsetTop && leftSection.scrollTop < (arti.offsetTop + arti.clientHeight)) {
+			arti.classList.add("big-red");
+		} else {
+			arti.classList.remove("big-red");
+		}
+	});
 
-
-
-
-
-
-
+	window.requestAnimationFrame(something);
+}
+something();
 
 
 
